355d014b267ad750ee5ac7d318ac18ebeb0996c3
EgummaWayne <hm6652@wayne.edu>
2024-02-14 16:13:44 -0500

    Update PokemonClass.py
    fixed type chart, damage calculator is officially done,
    no longer need a bunch of things for the function, just attacking pkm, defending pkm, and move done for simplicity

--- Code/PokemonClass.py
+++ Code/PokemonClass.py
@@ -1,8 +1,9 @@
 import random
 class Pokemon:
-    def __init__(self, name, types, hp, attack, defense, specialAttack, specialDefense, speed, level, moveset=None):
+    def __init__(self, name, type1, type2, hp, attack, defense, specialAttack, specialDefense, speed, level, moveset=None):
         self.name = name
-        self.types = types
+        self.type1 = type1
+        self.type2 = type2
         self.hp = hp
         self.attack = attack
         self.defense = defense
@@ -18,7 +19,8 @@ class Pokemon:
 
     def display_info(self):
         print(f"Name: {self.name}")
-        print(f"Types: {', '.join(self.types)}")
+        print(f"Type 1: {self.type1}")
+        print(f"Type 2: {self.type2}")
         print(f"HP: {self.hp}")
         print(f"Attack: {self.attack}")
         print(f"Defense: {self.defense}")
@@ -81,125 +83,139 @@ shock_wave = Move("Shock Wave", "Electric", "Special", 60, 100, 20)
 
 #Charizard's moveset
 charizard_moves = [flare_blitz, dragon_claw, air_slash, earthquake]
-charizard = Pokemon("Charizard", ["Fire", "Flying"], 78, 84, 78, 109, 85, 100, 50, charizard_moves)
+charizard = Pokemon("Charizard", "Fire", "Flying", 78, 84, 78, 109, 85, 100, 50, charizard_moves)
 
 # Snorlax's moveset
 snorlax_moves = [body_slam, earthquake, blizzard, earthquake]
-snorlax = Pokemon("Snorlax", ["Normal", "Null"], 160, 110, 65, 65, 110, 30, 50, snorlax_moves)
+snorlax = Pokemon("Snorlax", "Normal", "Null", 160, 110, 65, 65, 110, 30, 50, snorlax_moves)
 
 # Slowbro's moveset
 slowbro_moves = [scald, psychic_move, blizzard, ice_beam]
-slowbro = Pokemon("Slowbro", ["Water", "Psychic"], 95, 75, 110, 100, 80, 30, 50, slowbro_moves)
+slowbro = Pokemon("Slowbro", "Water", "Psychic", 95, 75, 110, 100, 80, 30, 50, slowbro_moves)
 
 # Swampert's moveset
 swampert_moves = [earthquake, waterfall, ice_punch, stone_edge]
-swampert = Pokemon("Swampert", ["Water", "Ground"], 100, 110, 90, 85, 90, 60, 50, swampert_moves)
+swampert = Pokemon("Swampert", "Water", "Ground", 100, 110, 90, 85, 90, 60, 50, swampert_moves)
 
 # Lickilicky's moveset
 lickilicky_moves = [body_slam, knock_off, power_whip, blizzard]
-lickilicky = Pokemon("Lickilicky", ["Normal", "Null"], 110, 85, 95, 80, 95, 50, 50, lickilicky_moves)
+lickilicky = Pokemon("Lickilicky", "Normal", "Null", 110, 85, 95, 80, 95, 50, 50, lickilicky_moves)
 
 # Mr. Mime's moveset
 mr_mime_moves = [psychic_move, dazzling_gleam, shock_wave, shadow_ball]
-mr_mime = Pokemon("Mr. Mime", ["Psychic", "Fairy"], 40, 45, 65, 100, 120, 90, 50, mr_mime_moves)
+mr_mime = Pokemon("Mr. Mime", "Psychic", "Fairy", 40, 45, 65, 100, 120, 90, 50, mr_mime_moves)
 
 # Blaziken's moveset
 blaziken_moves = [flare_blitz, high_jump_kick, knock_off, close_combat]
-blaziken = Pokemon("Blaziken", ["Fire", "Fighting"], 80, 120, 70, 110, 70, 80, 50, blaziken_moves)
+blaziken = Pokemon("Blaziken", "Fire", "Fighting", 80, 120, 70, 110, 70, 80, 50, blaziken_moves)
 
 #Sceptile's moveset
 sceptile_moves = [leaf_storm, dragon_pulse, focus_blast, energy_ball]
-sceptile = Pokemon("Sceptile", ["Grass", "Null"], 70, 85, 65, 105, 85, 120, 50, sceptile_moves)
+sceptile = Pokemon("Sceptile", "Grass", "Null", 70, 85, 65, 105, 85, 120, 50, sceptile_moves)
 
 # Electivire's moveset
 electivire_moves = [thunder_punch, ice_punch, earthquake, cross_chop]
-electivire = Pokemon("Electivire", ["Electric", "Null"], 75, 123, 67, 95, 85, 95, 50, electivire_moves)
+electivire = Pokemon("Electivire", "Electric", "Null", 75, 123, 67, 95, 85, 95, 50, electivire_moves)
 
 #Heracross's moveset
 heracross_moves = [close_combat, megahorn, stone_edge, knock_off]
-heracross = Pokemon("Heracross", ["Bug", "Fighting"], 80, 125, 75, 40, 95, 85, 50, heracross_moves)
+heracross = Pokemon("Heracross", "Bug", "Fighting", 80, 125, 75, 40, 95, 85, 50, heracross_moves)
 
 # Garchomp's moveset
 garchomp_moves = [earthquake, dragon_claw, stone_edge, fire_fang]
-garchomp = Pokemon("Garchomp", ["Dragon", "Ground"], 108, 130, 95, 80, 85, 102, 50, garchomp_moves)
+garchomp = Pokemon("Garchomp", "Dragon", "Ground", 108, 130, 95, 80, 85, 102, 50, garchomp_moves)
 
 #Mamoswine's moveset
 mamoswine_moves = [ice_shard, earthquake, icicle_crash, stone_edge]
-mamoswine = Pokemon("Mamoswine", ["Ice", "Ground"], 110, 130, 80, 70, 60, 80, 50, mamoswine_moves)
+mamoswine = Pokemon("Mamoswine", "Ice", "Ground", 110, 130, 80, 70, 60, 80, 50, mamoswine_moves)
 
 #Magmezone's moveset
 magnezone_moves = [thunderbolt, flash_cannon, tri_attack, zap_cannon]
-magnezone = Pokemon("Magnezone", ["Electric", "Steel"], 70, 70, 115, 130, 90, 60, 50, magnezone_moves)
+magnezone = Pokemon("Magnezone", "Electric", "Steel", 70, 70, 115, 130, 90, 60, 50, magnezone_moves)
 
 #Metagross's moveset
 metagross_moves = [meteor_mash, zen_headbutt, earthquake, bullet_punch]
-metagross = Pokemon("Metagross", ["Steel", "Psychic"], 80, 135, 130, 95, 90, 70, 50, metagross_moves)
+metagross = Pokemon("Metagross", "Steel", "Psychic", 80, 135, 130, 95, 90, 70, 50, metagross_moves)
 
 #Flygon's moveset
 flygon_moves = [earthquake, dragon_claw, fire_punch, stone_edge]
-flygon = Pokemon("Flygon", ["Ground", "Dragon"], 80, 100, 80, 80, 80, 100, 50, flygon_moves)
+flygon = Pokemon("Flygon", "Ground", "Dragon", 80, 100, 80, 80, 80, 100, 50, flygon_moves)
 
 #Lucario's moveset
 lucario_moves = [aura_sphere, flash_cannon, close_combat, extreme_speed]
-lucario = Pokemon("Lucario", ["Fighting", "Steel"], 70, 110, 70, 115, 70, 90, 50, lucario_moves)
+lucario = Pokemon("Lucario", "Fighting", "Steel", 70, 110, 70, 115, 70, 90, 50, lucario_moves)
 
 #Empoleon's moveset
 empoleon_moves = [hydro_pump, flash_cannon, ice_beam, earthquake]
-empoleon = Pokemon("Empoleon", ["Water", "Steel"], 84, 86, 88, 111, 101, 60, 50, empoleon_moves)
+empoleon = Pokemon("Empoleon", "Water", "Steel", 84, 86, 88, 111, 101, 60, 50, empoleon_moves)
 
 #Tyranitar's moveset
 tyranitar_moves = [stone_edge, crunch, earthquake, fire_punch]
-tyranitar = Pokemon("Tyranitar", ["Rock", "Dark"], 100, 134, 110, 95, 100, 61, 50, tyranitar_moves)
+tyranitar = Pokemon("Tyranitar", "Rock", "Dark", 100, 134, 110, 95, 100, 61, 50, tyranitar_moves)
 
 #Drapion's Moveset
 drapion_moves = [knock_off, poison_jab, earthquake, crunch]
-drapion = Pokemon("Drapion", ["Poison", "Dark"], 70, 90, 110, 60, 75, 95, 50, drapion_moves)
+drapion = Pokemon("Drapion", "Poison", "Dark", 70, 90, 110, 60, 75, 95, 50, drapion_moves)
 
 #Gengar's moveset
 gengar_moves = [shadow_ball, sludge_bomb, focus_blast, thunderbolt]
-gengar = Pokemon("Gengar", ["Ghost", "Poison"], 60, 65, 60, 130, 75, 110, 50, gengar_moves)
+gengar = Pokemon("Gengar", "Ghost", "Poison", 60, 65, 60, 130, 75, 110, 50, gengar_moves)
 
 #Scizor's moveset
 scizor_moves = [bullet_punch, body_slam, superpower, X_Scissor]
-scizor = Pokemon("Scizor", ["Bug", "Steel"], 70, 130, 100, 55, 80, 65, 50, scizor_moves)
+scizor = Pokemon("Scizor", "Bug", "Steel", 70, 130, 100, 55, 80, 65, 50, scizor_moves)
 
-def damagecalc(pkmLevel,movePower,pkmAttack,pkmDefense,pkmspeAttack,pkmspeDefense,attackType,pkmType1,pkmType2,moveType,deftype1,deftype2,moveAccuracy):
-    
+def damagecalc(attackingPokemon,defendingPokemon,Move):
+    pkmLevel = attackingPokemon.level
+    movePower = Move.power
+    pkmAttack = attackingPokemon.attack
+    pkmDefense = defendingPokemon.defense
+    pkmspeAttack = attackingPokemon.specialAttack
+    pkmspeDefense = defendingPokemon.specialDefense
+    attackType = Move.attacktype
+    attackingType1 = attackingPokemon.type1
+    attackingType2 = attackingPokemon.type2
+    moveType = Move.move_type
+    defendingType1 = defendingPokemon.type1
+    defendingType2 = defendingPokemon.type2
+    moveAccuracy = Move.accuracy
+
     hits = (random.randint(0, 100))
     if hits <= moveAccuracy:
         type_effectiveness = {
             'Normal': {'Fighting': 2, 'Ghost': 0},
-            'Fire': {'Fire': 0.5, 'Water': 0.5, 'Grass': 2, 'Ice': 2, 'Bug': 2, 'Rock': 0.5, 'Dragon': 0.5, 'Steel': 2},
-            'Water': {'Fire': 2, 'Water': 0.5, 'Grass': 0.5, 'Ground': 2, 'Rock': 2, 'Dragon': 0.5},
-            'Electric': {'Water': 2, 'Electric': 0.5, 'Grass': 0.5, 'Ground': 0, 'Flying': 2, 'Dragon': 0.5},
-            'Grass': {'Fire': 0.5, 'Water': 2, 'Grass': 0.5, 'Poison': 0.5, 'Ground': 2, 'Flying': 0.5, 'Bug': 0.5, 'Rock': 2, 'Dragon': 0.5, 'Steel': 0.5},
-            'Ice': {'Fire': 0.5, 'Water': 0.5, 'Grass': 2, 'Ice': 0.5, 'Ground': 2, 'Flying': 2, 'Dragon': 2, 'Steel': 0.5},
-            'Fighting': {'Normal': 2, 'Ice': 2, 'Poison': 0.5, 'Flying': 0.5, 'Psychic': 0.5, 'Bug': 0.5, 'Rock': 2, 'Ghost': 0, 'Dark': 2, 'Steel': 2, 'Fairy': 0.5},
-            'Poison': {'Grass': 2, 'Poison': 0.5, 'Ground': 0.5, 'Rock': 0.5, 'Ghost': 0.5, 'Steel': 0, 'Fairy': 2},
-            'Ground': {'Fire': 2, 'Electric': 2, 'Grass': 0.5, 'Poison': 2, 'Flying': 0, 'Bug': 0.5, 'Rock': 2, 'Steel': 2},
-            'Flying': {'Electric': 0.5, 'Grass': 2, 'Fighting': 2, 'Bug': 2, 'Rock': 0.5, 'Steel': 0.5},
-            'Psychic': {'Fighting': 2, 'Poison': 2, 'Psychic': 0.5, 'Dark': 0, 'Steel': 0.5},
-            'Bug': {'Fire': 0.5, 'Grass': 2, 'Fighting': 0.5, 'Poison': 0.5, 'Flying': 0.5, 'Psychic': 2, 'Ghost': 0.5, 'Dark': 2, 'Steel': 0.5, 'Fairy': 0.5},
-            'Rock': {'Fire': 2, 'Ice': 2, 'Fighting': 0.5, 'Ground': 0.5, 'Flying': 2, 'Bug': 2, 'Steel': 0.5},
-            'Ghost': {'Normal': 0, 'Psychic': 2, 'Ghost': 2, 'Dark': 0.5},
-            'Dragon': {'Dragon': 2, 'Steel': 0.5, 'Fairy': 0},
-            'Dark': {'Fighting': 0.5, 'Psychic': 2, 'Ghost': 2, 'Dark': 0.5, 'Fairy': 0.5},
-            'Steel': {'Fire': 0.5, 'Water': 0.5, 'Electric': 0.5, 'Ice': 2, 'Rock': 2, 'Steel': 0.5, 'Fairy': 2},
-            'Fairy': {'Fighting': 2, 'Poison': 0.5, 'Steel': 0.5, 'Fire': 0.5, 'Dragon': 2, 'Dark': 2},
-            'Null' : {}
+            'Fire': {'Water': 2, 'Grass': 0.5, 'Ice': 0.5, 'Bug': 0.5, 'Steel': 0.5, 'Fire': 0.5, 'Rock': 2, 'Dragon': 0.5},
+            'Water': {'Electric': 2, 'Grass': 2, 'Fire': 0.5, 'Water': 0.5, 'Ice': 0.5, 'Steel': 0.5},
+            'Electric': {'Ground': 2, 'Electric': 0.5, 'Flying': 0.5, 'Steel': 0.5},
+            'Grass': {'Fire': 2, 'Ice': 2, 'Poison': 2, 'Flying': 2, 'Bug': 2, 'Water': 0.5, 'Grass': 0.5, 'Electric': 0.5, 'Ground': 0.5},
+            'Ice': {'Fire': 2, 'Fighting': 2, 'Rock': 2, 'Steel': 2, 'Ice': 0.5},
+            'Fighting': {'Flying': 2, 'Psychic': 2, 'Fairy': 2, 'Bug': 0.5, 'Rock': 0.5, 'Dark': 0.5},
+            'Poison': {'Ground': 2, 'Psychic': 2, 'Grass': 0.5, 'Fighting': 0.5, 'Poison': 0.5, 'Bug': 0.5, 'Fairy': 0.5},
+            'Ground': {'Water': 2, 'Grass': 2, 'Ice': 2, 'Poison': 0.5, 'Rock': 0.5, 'Electric': 0},
+            'Flying': {'Electric': 2, 'Ice': 2, 'Rock': 2, 'Grass': 0.5, 'Fighting': 0.5, 'Bug': 0.5, 'Ground': 0},
+            'Psychic': {'Bug': 2, 'Ghost': 2, 'Dark': 2, 'Fighting': 0.5, 'Psychic': 0.5},
+            'Bug': {'Fire': 2, 'Flying': 2, 'Rock': 2, 'Grass': 0.5, 'Fighting': 0.5, 'Ground': 0.5},
+            'Rock': {'Water': 2, 'Grass': 2, 'Fighting': 2, 'Ground': 2, 'Steel': 2, 'Fire': 0.5, 'Ice': 0.5, 'Flying': 0.5, 'Bug': 0.5},
+            'Ghost': {'Ghost': 2, 'Dark': 2, 'Bug': 0.5, 'Poison': 0.5, 'Normal': 0, 'Fighting': 0},
+            'Dragon': {'Ice': 2, 'Dragon': 2, 'Fairy': 2, 'Fire': 0.5, 'Water': 0.5, 'Electric': 0.5, 'Grass': 0.5},
+            'Dark': {'Fighting': 2, 'Bug': 2, 'Fairy': 2, 'Ghost': 0.5, 'Psychic': 0},
+            'Steel': {'Fire': 2, 'Fighting': 2, 'Ground': 2, 'Poison': 0, 'Flying': 0.5, 'Rock': 0.5, 'Bug': 0.5, 'Steel': 0.5, 'Grass': 0.5, 'Psychic': 0.5, 'Ice': 0.5, 'Dragon': 0.5, 'Fairy': 0.5},
+            'Fairy': {'Poison': 2, 'Steel': 2, 'Fighting': 0.5, 'Bug': 0.5, 'Dark': 0.5, 'Dragon': 0},
+            'Null': {}
         }
-        #Checks Second Type
-        if deftype1 in type_effectiveness and moveType in type_effectiveness[deftype1]:
-            TYPE1 = type_effectiveness[deftype1][moveType]
+
+        #Checks First Type
+        if defendingType1 in type_effectiveness and moveType in type_effectiveness[defendingType1]:
+            TYPE1 = type_effectiveness[defendingType1][moveType]
         else:
             TYPE1 = 1
-        #Checks first Type
-        if deftype2 in type_effectiveness and moveType in type_effectiveness[deftype2]:
-            TYPE2 = type_effectiveness[deftype2][moveType]
+        #Checks Second Type
+        if defendingType2 in type_effectiveness and moveType in type_effectiveness[defendingType2]:
+            TYPE2 = type_effectiveness[defendingType2][moveType]
         else:
             TYPE2 = 1
         #checks stab
-        if pkmType1 == moveType or (pkmType2 == moveType and pkmType2 != "Null"):
+        if attackingType1 == moveType or (attackingType2 == moveType and attackingType2 != "Null"):
             STAB = 1.5
         else:
             STAB = 1
@@ -217,14 +233,9 @@ def damagecalc(pkmLevel,movePower,pkmAttack,pkmDefense,pkmspeAttack,pkmspeDefens
             damageDone = ((((((pkmLevel*2)/5)+2)*movePower*(pkmAttack/pkmDefense))/50)+2)*STAB*TYPE1*TYPE2*randdam*critical
         elif attackType == "Special":
             damageDone = ((((((pkmLevel*2)/5)+2)*movePower*(pkmspeAttack/pkmspeDefense))/50)+2)*STAB*TYPE1*TYPE2*randdam*critical
-        return damageDone
+        return int(damageDone)
     else:
         return 0
 
-print(damagecalc(50, 40, 130, 60, 55, 75,"Physical", "Bug", "Steel", "Steel", "Ghost", "Poison",100))
-print(damagecalc(50, 120, 130, 60, 55, 75, "Special", "Fire", "Null", "Fire", "Grass", "Null",70))
-print(damagecalc(50, 80, 100, 100, 55, 75, "Physical", "Normal", "Null", "Normal", "Normal", "Null",50))
-print(damagecalc(50, 100, 130, 60, 55, 75, "Special", "Electric", "Null", "Electric", "Ground", "Null",20))
-print(damagecalc(50, 90, 120, 60, 100, 90, "Special", "Psychic", "Null", "Psychic", "Fighting", "Null",100))
-print(damagecalc(50, 75, 110, 70, 55, 75, "Physical", "Water", "Ice", "Water", "Rock", "Fire",60))
-print(damagecalc(50, 10, 50, 200, 55, 75, "Physical", "Bug", "Null", "Bug", "Steel", "Null",90))
+print(damagecalc(scizor,gengar,bullet_punch))
+print(damagecalc(drapion,snorlax,poison_jab))
