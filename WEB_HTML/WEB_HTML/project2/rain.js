f60f0667ccdf5e35a9ab96dde3cf248391f51c38
ggsbada02 <126417397+ggsbada02@users.noreply.github.com>
2024-03-04 21:08:58 -0500

    Update rain.js
    change ../pic/project2pic/

--- WEB_HTML/WEB_HTML/project2/rain.js
+++ WEB_HTML/WEB_HTML/project2/rain.js
@@ -1,9 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var imagePaths = ["../pokemon/img/01.png", "../pokemon/img/02.png","../pokemon/img/03.png","../pokemon/img/04.png", "../pokemon/img/05.png",
-        "../pokemon/img/06.png", "../pokemon/img/07.png", "../pokemon/img/08.png", "../pokemon/img/09.png", "../pokemon/img/10.png",
-        "../pokemon/img/11.png", "../pokemon/img/12.png", "../pokemon/img/13.png", "../pokemon/img/14.png", "../pokemon/img/15.png",
-        "../pokemon/img/16.png", "../pokemon/img/17.png", "../pokemon/img/18.png", "../pokemon/img/19.png", "../pokemon/img/20.png",
-        "../pokemon/img/pikachu.png"];
+    var imagePaths = ["../pic/project2pic/01.png", "../pic/project2pic/02.png","../pic/project2pic/03.png","../pic/project2pic/04.png", "../pic/project2pic/05.png",
+        "../pic/project2pic/06.png", "../pic/project2pic/07.png", "../pic/project2pic/08.png", "../pic/project2pic/09.png", "../pic/project2pic/10.png",
+        "../pic/project2pic/11.png", "../pic/project2pic/12.png", "../pic/project2pic/13.png", "../pic/project2pic/14.png", "../pic/project2pic/15.png",
+        "../pic/project2pic/16.png", "../pic/project2pic/17.png", "../pic/project2pic/18.png", "../pic/project2pic/19.png", "../pic/project2pic/20.png"];
 
     var container = document.getElementById("raindrop-container");
 
